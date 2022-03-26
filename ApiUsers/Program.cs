d5be9718df7cbbffbd6e739631cf088bf8e0737f
Isaac-maker <65924751+Isaac-maker@users.noreply.github.com>
2022-03-26 17:04:46 -0600

    dos cros

--- ApiUsers/Program.cs
+++ ApiUsers/Program.cs
@@ -3,8 +3,17 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder =>
+     {
+         builder
+         .AllowAnyMethod()
+         .AllowAnyHeader()
+         .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+     });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( SwaggerGenOptions=>
 {
@@ -22,6 +31,7 @@ app.UseSwaggerUI( SwaggerUIOptionS =>
 });
 
 app.UseHttpsRedirection();
+app.UseCors("CORSPolicy");
 
 
 app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync()).WithTags("Posts Endpoints");
